c24db10f94ada7c5171f468a579b13a29db693c6
bidi <baduc.nguyen1992@gmail.com>
2021-05-12 22:23:27 +0700

    add gitignore solve twosum 2

--- src/com/bidi/problem/TwoSum.java
+++ src/com/bidi/problem/TwoSum.java
@@ -3,23 +3,36 @@ package com.bidi.problem;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://leetcode.com/problems/two-sum/
+ * 
+ * @author bidi
+ *
+ *         Given an array of integers nums and an integer target, return indices
+ *         of the two numbers such that they add up to target.
+ * 
+ *         You may assume that each input would have exactly one solution, and
+ *         you may not use the same element twice.
+ * 
+ *         You can return the answer in any order.
+ */
 public class TwoSum {
 
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-               return new int[] {i, map.get(nums[i])};
-            } else {
-                int numberToFind = target - nums[i];
-                map.put(numberToFind, i);
-            }
-        }
-        
-        return null;
-    }
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				return new int[] { i, map.get(nums[i]) };
+			} else {
+				int numberToFind = target - nums[i];
+				map.put(numberToFind, i);
+			}
+		}
+
+		return null;
+	}
 }
---
+++ src/com/bidi/problem/TwoSum2.java
@@ -0,0 +1,55 @@
+package com.bidi.problem;
+
+/**
+ * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ * 
+ * @author bidi
+ *
+ *         Given an array of integers numbers that is already sorted in
+ *         ascending order, find two numbers such that they add up to a specific
+ *         target number.
+ * 
+ *         Return the indices of the two numbers (1-indexed) as an integer array
+ *         answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+ * 
+ *         You may assume that each input would have exactly one solution and
+ *         you may not use the same element twice.
+ * 
+ */
+public class TwoSum2 {
+
+	public static void main(String[] args) {
+		int[] t1 = new int[] { 2, 7, 11, 15 };
+		int k1 = 9;
+
+		int[] t2 = new int[] { 2, 3, 4 };
+		int k2 = 6;
+
+		int[] t3 = new int[] { -1, 0 };
+		int k3 = -1;
+
+		int[] twoSum = twoSum(t3, k3);
+		System.out.println(twoSum[0]);
+		System.out.println(twoSum[1]);
+	}
+
+	public static int[] twoSum(int[] numbers, int target) {
+
+		int low = 1;
+		int high = numbers.length;
+
+		while (low < high) {
+			int sum = numbers[low - 1] + numbers[high - 1];
+			if (sum == target) {
+				return new int[] { low, high };
+			} else if (sum > target) {
+				high--;
+			} else {
+				low++;
+			}
+
+		}
+
+		return null;
+	}
+}
